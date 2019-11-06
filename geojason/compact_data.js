4555d08189a00aa23fd2b4c563da9ef204dda1c6
Paul <cjan499@aucklanduni.ac.nz>
2019-11-07 00:26:40 +1300

    Geojason Test

---
+++ geojason/compact_data.js
@@ -0,0 +1,189 @@
+var data = {"type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [  174.7569,-36.8461  ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [  174.757,-36.846  ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+    {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.757,-36.846 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7568,-36.846 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+	{
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7568,-36.846 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7568,-36.846 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7567,-36.846 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7567,-36.846 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7569,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7569,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.757,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.757,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.757,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7571,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7571,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7571,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 174.7571,-36.8459 ]
+    },
+    "properties": {
+    "Business Unit":"Corporate Accomodation",
+    "Location":"20 Viaduct Harbour Avenue"
+    }
+  }]};
